888b1d9699344565625d8bee66bdd18d5156ba0f
KikyTokamuro <kiky.tokamuro@yandex.ru>
2022-12-20 19:28:35 +0300

    Added new button for open 'New task' modal

--- index.lisp
+++ index.lisp
@@ -16,9 +16,11 @@
       (:link :rel "stylesheet" :href "./static/style.css")
       (:link :rel "stylesheet" :href "./static/libs/jquery-ui.css")
       (:link :rel "stylesheet" :href "./static/libs/liner-bar.css")
+      (:link :rel "stylesheet" :href "./static/libs/quill.snow.min.css")
       (:link :rel "icon" :type "image/x-icon" :href "./static/images/favicon.ico")
       (:script :src "./static/libs/jquery.min.js")
       (:script :src "./static/libs/jquery-ui.min.js")
+      (:script :src "./static/libs/quill.min.js")
       (:script :src "./static/libs/liner-bar.js"))
      (:body
       (:div :class "todolist-wrapper"
@@ -44,12 +46,16 @@
 		  (:div :class "todolist-done-column"
 			(:div :class "todolist-done-column-title" "DONE")
 			(:div :class "column todolist-done-column-body")))
-	    (:div :class "todolist-input-wrapper"
-		  (:textarea :id "task-input" :rows "1" :placeholder "Insert task")
-		  (:div :class "separator")
-		  (:input :type "text" :id "task-group" :placeholder "Insert task group")
-		  (:div :class "separator")
-		  (:div :class "send-task-button"
-			(:img :src "./static/images/send.svg"))))
+	    (:div :class "todolist-create-button"
+		  (:img :src "./static/images/pencil.svg")))
+	    ;; (:div :class "todolist-input-wrapper"
+	    ;; 	  (:textarea :id "task-input" :rows "1" :placeholder "Insert task")
+	    ;; 	  (:div :class "separator")
+	    ;; 	  (:input :type "text" :id "task-group" :placeholder "Insert task group")
+	    ;; 	  (:div :class "separator")
+	    ;; 	  (:div :class "send-task-button"
+	    ;; 		(:img :src "./static/images/send.svg"))))
+      (:div :class "todolist-create-task-modal" :style "display:none" :title "New task")
+      (:div :class "todolist-edit-task-modal" :style "display:none" :title "Edit task")
       (:div :class "todolist-statistics-modal" :style "display:none" :title "Statistics")
       (:script :type "module" :src "./static/app.js")))))
